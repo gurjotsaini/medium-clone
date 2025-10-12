d2547bb8e1c6c7af4b5097a85c969f2f59e4d797
gurjotsaini <gurjotsaini1992@gmail.com>
2025-10-11 22:13:44 -0400

    Add: index method to post controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    @foreach($categories as $category)
+                        <div class="mt-4 p-4 border rounded-lg">
+                            <h3 class="text-lg font-semibold"><a href="">{{ $category->name }}</a></h3>
+                        </div>
+
+                    @endforeach
                 </div>
             </div>
         </div>
